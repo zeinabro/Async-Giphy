3c24060c6edbcf6075a2ca7afbb0f1bdf843d338
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-13 10:49:51 +0100

    function to return random image from api with query

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 require('dotenv').config();
 
+async function getImages(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    const res = await fetch(endpoint)
+    const data = await res.json()
+    let num = Math.floor(Math.random()*25)
+    const url = data.data[num].images.original.url
+    console.log(url)
+    return url
+}
+
+getImages("dogs")
+
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
