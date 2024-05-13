77c64eb5491311c31fa21542a65b96f11c1feb67
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-13 11:39:22 +0100

    html page with form to search query and output gifs

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-require('dotenv').config();
+// require('dotenv').config()
 
 async function getImages(query){
-    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=S3fQysCpDBrhmpoJDMJ9oi9500WsmzNn&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
     const res = await fetch(endpoint)
     const data = await res.json()
-    let num = Math.floor(Math.random()*25)
-    const url = data.data[num].images.original.url
-    console.log(url)
-    return url
+
+    const imageObj = data.data[Math.floor(Math.random()*25)]
+    const url = imageObj.images.original.url
+
+    return {imageObj: imageObj,url: url}
 }
 
-async function helperFunction(){
-    await getImages("dogs")
+async function addImage(query){
+    if (image_div.childNodes){
+        image_div.removeChild(image_div.childNodes[0])
+    }
+
+    const result = await getImages(query)
+
+    const image = document.createElement("img")
+    image.src=result.url
+    image.alt=result.imageObj.alt_text
+    image.width=300
+    image.height=300
+
+    image_div.appendChild(image)
 }
 
-helperFunction()
+const form = document.querySelector("#query-form")
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let form_data = new FormData(form)
+    let query = form_data.get('query')
+    addImage(query)
+})
+
+const image_div = document.querySelector(".images")
